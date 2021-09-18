510f5a724c0fd402fd9bb8678e26ac5a2c7af755
gmmmmd <zoopark86@yandex.ru>
2021-09-18 18:12:26 +0400

    ДЗ 1-2

---
+++ src/pages/Homework/homework-1.js
@@ -0,0 +1,13 @@
+
+
+let name = prompt('Введите ваше имя');
+let surname = prompt('Введите вашу фамилию')
+let age = +prompt('Введите ваш возраст');
+let user = {
+    name: name,
+    surname: surname,
+    age: age
+};
+console.log(user)
+
+
